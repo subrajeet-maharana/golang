5663139635326f255e3b35173d167486176b14e7
subrajeet-maharana <subrajeet.info@gmail.com>
2025-01-01 21:15:48 +0530

    Added Show All Task function in Todo CLI

--- 31projects/04todoCLI/main.go
+++ 31projects/04todoCLI/main.go
@@ -8,10 +8,28 @@ type Todo struct {
   Completed bool
 }
 
+func showAllTasks(todos []Todo){
+  fmt.Println("All Tasks")
+  for _, todo := range todos {
+    fmt.Printf("ID: %d | Task: %s | ", todo.ID, todo.Task)
+    var completionStatus string = "Completed"
+    if(!todo.Completed) {
+      completionStatus ="In Progress"
+    }
+    fmt.Printf("Status: %s\n", completionStatus)
+  }
+}
+
 func main() { 
   fmt.Println("Welcome to the TO-DO CLI App")
   var todos []Todo
   for {
+    var option int
     fmt.Printf("\nPlease choose an option:\n1. View All Tasks\n2. Add a new Task\n3. Mark a task as Completed\n4. Delete a Task\n5. Exit\n")
+    fmt.Scanln(option)
+    switch option {
+    case 1:
+      showAllTasks(todos)
+    }
   }
 }
