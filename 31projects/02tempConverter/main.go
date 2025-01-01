857da72afb94c0501d7b04cf3815a442c583ea3b
subrajeet-maharana <subrajeet.info@gmail.com>
2025-01-01 15:48:56 +0530

    Added f to c

--- 31projects/02tempConverter/main.go
+++ 31projects/02tempConverter/main.go
@@ -5,8 +5,15 @@ import "fmt"
 func celsiusToFahrenheit(temp float32) float32 {
   return (9*temp/5)+32
 }
+
+func fahrenheitToCelsius(temp float32) float32 {
+  return ((temp-32)*5)/9
+}
+
 func main(){
   fmt.Println("Welcome to our Temperature Converter")
   fmt.Println("Temp in Fahrenheit is: ", celsiusToFahrenheit(86))
   fmt.Println("Temp in Fahrenheit is: ", celsiusToFahrenheit(-40))
+  fmt.Println("Temp in Celsius is: ", fahrenheitToCelsius(86))
+  fmt.Println("Temp in Celsius is: ", fahrenheitToCelsius(-40))
 }
