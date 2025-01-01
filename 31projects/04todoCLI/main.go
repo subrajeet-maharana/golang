311e43a817aee2d3cca92ce67de1885234346f55
subrajeet-maharana <subrajeet.info@gmail.com>
2025-01-01 22:08:54 +0530

    implemented mark as completed in ToDo CLI

--- 31projects/04todoCLI/main.go
+++ 31projects/04todoCLI/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type Todo struct {
   ID int
@@ -8,21 +12,23 @@ type Todo struct {
   Completed bool
 }
 
-func showAllTasks(todos []Todo){
+var todos []Todo
+
+func showAllTasks(){
   fmt.Println("All Tasks")
   for _, todo := range todos {
-    fmt.Printf("ID: %d | Task: %s | ", todo.ID, todo.Task)
     var completionStatus string = "Completed"
     if(!todo.Completed) {
       completionStatus ="In Progress"
     }
-    fmt.Printf("Status: %s\n", completionStatus)
+    fmt.Printf("ID: %d | Task: %s | Status: %s\n",todo.ID, todo.Task, completionStatus)
   }
 }
 
-func addNewTask(todos []Todo) {
-  var task string
-  fmt.Scanln("Add task: ", &task)
+func addNewTask() {
+  fmt.Printf("Add task: ")
+  reader:=bufio.NewReader(os.Stdin)
+  task, _ :=reader.ReadString('\n')
   newTodo := Todo{
     ID: len(todos),
     Task: task,
@@ -32,18 +38,27 @@ func addNewTask(todos []Todo) {
   fmt.Println("Todo added successfully...")
 }
 
+func markAsCompleted() {
+  var id int
+  fmt.Println("Enter the ID of the task: ")
+  fmt.Scanln(&id)
+  todos[id].Completed=true
+  fmt.Printf("Todo with ID: %d marked as completed..", id)
+}
+
 func main() { 
   fmt.Println("Welcome to the TO-DO CLI App")
-  var todos []Todo
   for {
     var option int
     fmt.Printf("\nPlease choose an option:\n1. View All Tasks\n2. Add a new Task\n3. Mark a task as Completed\n4. Delete a Task\n5. Exit\n")
-    fmt.Scanln(option)
+    fmt.Scanln(&option)
     switch option {
     case 1:
-      showAllTasks(todos)
+      showAllTasks()
     case 2:
-      addNewTask(todos)
+      addNewTask()
+    case 3:
+      markAsCompleted()
     }
   }
 }
