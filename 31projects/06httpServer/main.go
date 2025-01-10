09a4d06738b648f9488c86f6c35a6d412eaf4f64
subrajeet-maharana <subrajeet.info@gmail.com>
2025-01-10 13:20:37 +0530

    added postsHandler

--- 31projects/06httpServer/main.go
+++ 31projects/06httpServer/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"sync"
 )
 
@@ -24,6 +25,23 @@ func postsHandler(w http.ResponseWriter, r *http.Request) {
     handleGetPosts(w, r)
   case "POST":
     handlePostPosts(w, r)
+  default:
+    http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
+  }
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+  id, err := strconv.Atoi(r.URL.Path[len("/posts/"):])
+  if err != nil {
+    http.Error(w, "Invalid ID", http.StatusBadRequest)
+    return 
+  }
+
+  switch r.Method {
+  case "GET":
+    handleGetPost(w, r, id)
+  case "DELETE":
+    handleDeletePost(w, r, id)
   default:
     http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
   }
