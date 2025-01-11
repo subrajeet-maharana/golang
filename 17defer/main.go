4a76d1d2f27fc88c5a88a5a84c970f8395abb433
subrajeet-maharana <subrajeet.info@gmail.com>
2025-01-11 15:09:39 +0530

    Recover and panic function in defer

--- 17defer/main.go
+++ 17defer/main.go
@@ -7,10 +7,24 @@ func main() {
 	defer fmt.Println("Two")
 	defer fmt.Println("Three")
 	fmt.Println("Four")
-	myDefer()
+	// myDefer()
+  panicAndRecover()
 }
 func myDefer() {
 	for i := 0; i < 5; i++ {
 		defer fmt.Printf("%v ", i)
 	}
+}
+
+func panicAndRecover() {
+  fmt.Println("Panic and Recover Demonstration: ")
+  defer func() {
+    if r := recover(); r != nil {
+      fmt.Println("Recovered from panic!", r)
+    }
+  }()
+
+  fmt.Println("Before Panic: ")
+  panic("Something went wrong...")
+  fmt.Println("After Panic: ")
 }
